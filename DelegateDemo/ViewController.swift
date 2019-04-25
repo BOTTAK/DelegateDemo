bc84a3a3f3b1d5fb1f85eeb87df868f36b66821a
bottak <djbottak@gmail.com>
2019-04-25 09:45:38 +0300

    Initial Commit

---
+++ DelegateDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DelegateDemo
+//
+//  Created by BOTTAK on 4/25/19.
+//  Copyright © 2019 BOTTAK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
